d934ca6b2cacc14a7d7b3624fc4eda1c340b2926
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2020-04-05 11:04:50 +0530

    Add files via upload

---
+++ 22app.js
@@ -0,0 +1,34 @@
+//
+// Goal: Create method to get incomplete tasks
+//
+// 1. Define getTasksToDo method
+// 2. Use filter to to return just the incompleted tasks (arrow function)
+// 3. Test your work by running the script
+
+const tasks = {
+    tasks: [{
+        text: 'Grocery shopping',
+        completed: true
+    },{
+        text: 'Clean yard',
+        completed: false
+    }, {
+        text: 'Film course',
+        completed: false
+    }],
+
+     retincomp(){
+        return this.tasks.filter((tasks)=>  tasks.completed === false
+        )    
+    }
+}
+
+console.log(tasks.retincomp())
+const getTasksToDo =tasks.tasks.filter((tasks)=>{
+    if(tasks.completed===false)
+    return tasks
+})
+
+
+
+console.log(getTasksToDo)
